78676c01e5aa454c679c5e3c04f7ebdb21d914cd
AcevedoAdrian <acevedohugoadrian@gmail.com>
2022-09-30 08:12:44 -0300

    New custom hook which allows create form custom

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client';
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/CounterApp';
 // import { CounterWithCustomeHook } from './useState/CounterWithCustomeHook';
-import { SimpleForm } from './useEffect/SimpleForm';
+// import { SimpleForm } from './useEffect/SimpleForm';
+import { FormWithCustomHook } from './useEffect/FormWithCustomHook';
+
 
 import './index.css';
 
@@ -13,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <HookApp /> */}
     {/* <CounterApp /> */}
     {/* <CounterWithCustomeHook /> */}
-    <SimpleForm />
+    {/* <SimpleForm /> */}
+    <FormWithCustomHook />
   </React.StrictMode>
 )
